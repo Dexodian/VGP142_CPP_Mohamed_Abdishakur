9236b766dfa6ef2b64b42b7e25c34b0fdd904fb8
Dexodian <ashakur789@gmail.com>
2022-07-20 21:44:14 -0400

    Lab 02 Portion_03

---
+++ Assets/Script/Player/Character.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[RequireComponent(typeof(CharacterController))]
+public class Character : MonoBehaviour
+{
+    CharacterController controller;
+
+    [Header("Player Settings")]
+    [Space(10)]
+    [Tooltip("Speed value between 1 and 20")]
+    [Range(1.0f, 20.0f)]
+    public float speed = 20;
+    public float gravity = 9.8f;
+    public float jumpSpeed = 10.0f;
+
+    Vector3 moveDir;
+
+    [Header("Weapon Settings")]
+    [Space(10)]
+    public float projectileForce = 10.0f;
+    public Rigidbody projectilePrefab;
+    public Transform projectileSpawnPoint;
+    public Transform projectileSpawnPointLeft;
+    public Transform projectileSpawnPointRight;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        controller.minMoveDistance = 0.0f;
+
+        if (speed <= 0.0f)
+        {
+            speed = 6.0f;
+
+        }
+
+        if (jumpSpeed <= 0)
+        {
+            jumpSpeed = 10.0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        try
+        {
+            if (controller.isGrounded)
+
+            {
+                moveDir = new Vector3(horizontal, 0.0f, vertical);
+                moveDir *= speed;
+
+                moveDir = transform.TransformDirection(moveDir);
+
+                if (Input.GetButtonDown("Jump"))
+                {
+                    moveDir.y = jumpSpeed;
+                }
+
+            }
+
+            moveDir.y -= gravity * Time.deltaTime;
+            controller.Move(moveDir * Time.deltaTime);
+
+            if (Input.GetButtonDown("Fire1"))
+                Fire();
+            throw new UnassignedReferenceException("Shooting Disabled " + name + " Shooting Enabled " + Input.GetButtonDown("Fire1"));
+        }
+        finally
+        {
+            Debug.Log("Shooting has been Enabled");
+        }
+    }
+
+    void Fire()
+    {
+        if (projectilePrefab && projectileSpawnPoint)
+        {
+            Rigidbody temp = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
+
+            temp.AddForce(projectileSpawnPoint.forward * projectileForce, ForceMode.Impulse);
+
+            Destroy(temp.gameObject, 2.0f);
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "Coin")
+        {
+            Destroy(hit.gameObject);
+        }
+    }
+}
