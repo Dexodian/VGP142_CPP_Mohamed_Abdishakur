fdc4b075588e5f2f23609287b42e188e25d038c2
Dexodian <ashakur789@gmail.com>
2022-07-25 16:10:52 -0400

    Lab 03 Portion_02

---
+++ Assets/Script/Manager/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EndGame()
+    {
+
+    }
+}
