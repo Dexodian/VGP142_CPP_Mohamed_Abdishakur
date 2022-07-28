da706759b5729b87a605b229ec4605134cd13bc0
Abdi_M <ashakur789@gmail.com>
2022-07-28 16:03:29 -0400

    Lab 03 Portion_08

---
+++ Assets/Script/Enemy/Mechanics/CheckForPlayer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckForPlayer : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float sightDistance = 100f;
+    public Transform originPoint;
+    public float speed;
+    public float rotationSpeed;
+    private Vector3 playerPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playerTransform)
+            transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        else if (speed < 0)
+            transform.rotation = playerTransform.rotation;
+        else
+            transform.LookAt(playerPosition);
+
+        RaycastHit hit;
+        if (Physics.Raycast(originPoint.position, transform.forward, out hit, sightDistance))
+        {
+            if (hit.transform.gameObject.tag == "Player")
+            {
+                playerTransform = hit.transform;
+
+            }
+        }
+
+        Vector3 dir = transform.TransformDirection(Vector3.forward) * sightDistance;
+        
+        Debug.DrawRay(originPoint.position, dir, Color.red);
+
+        if (playerTransform)
+        {
+            playerPosition = new Vector3(playerTransform.position.x, 30f, playerTransform.position.z);
+            
+            transform.position = Vector3.MoveTowards(transform.position, playerPosition, speed);
+        }
+
+
+    }
+}
